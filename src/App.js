e23ad1e99098dca392c690581f3ba9e6ca153c89
ramii845 <rami.elfrikha@gmail.com>
2021-11-20 14:05:41 +0100

    state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
+import React,{Component} from 'react';
+class App extends React.Component{
+  state={
+  data:[{
+    fullName:"Rami",
+   profession:"student",
+   bio:"sfax",
+    bol:true,
+    image:<img src="img1.jpg" alt="image" height={150} width={150}/>,
+    id:0
+  },
+  {
+fullName:"sami",
+profession:"teacher",
+bio:"tunis",
+image:<img src="img1.jpg" alt="image" height={150} width={150}/>,
+bol:false,
+id:0
+  },
+]};
+handclick=()=> this.setState({
+    data:{id:this.state.id+1}
+  });
+ handshow=()=> this.setState({
+  data:{bol:!this.state.data.bol}
+});
+render(){
+return(
+  <div className="c1">
+    <button onClick={this.handshow}> cliquer pour changement </button>
+      {this.state.data[0] && 
+      <div>
+    {this.state.data.map((el)=>
+    <div>
+     <h1>{el.fullName} </h1>
+     <h1>{el.profession}</h1>
+    <h1>{el.bio}</h1>
+    <h1> {el.id}</h1>
+    <h1>{el.image}</h1>
+    </div>)}
+    </div>  }
+    <button onClick={this.handclick}> cliquer pour avancement</button>
+    </div> 
+)
+    };
+};
 export default App;
+
+
